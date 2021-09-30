728c50add30595d3c7b07dc15ac9147d5ab33e03
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:38:28 +0200

    create createExpense()

--- src/main/java/ch/zli/m223/kti/punchclock/service/ExpenseService.java
+++ src/main/java/ch/zli/m223/kti/punchclock/service/ExpenseService.java
@@ -1,11 +1,21 @@
 package ch.zli.m223.kti.punchclock.service;
 
+import ch.zli.m223.kti.punchclock.domain.Entry;
+import ch.zli.m223.kti.punchclock.domain.Expense;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 public class ExpenseService {
 
     @Inject
     private EntityManager entityManager;
+
+    @Transactional
+    public Expense createExpense(Expense expense) {
+        entityManager.persist(expense);
+        return expense;
+    }
 
 }
