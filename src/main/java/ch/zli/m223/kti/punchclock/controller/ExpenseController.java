52a8455d3e0be7bf41b61d6b6285a32252ec5876
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 09:01:37 +0200

    created list() for ExpenseController

--- src/main/java/ch/zli/m223/kti/punchclock/controller/ExpenseController.java
+++ src/main/java/ch/zli/m223/kti/punchclock/controller/ExpenseController.java
@@ -1,10 +1,18 @@
 package ch.zli.m223.kti.punchclock.controller;
 
+import ch.zli.m223.kti.punchclock.domain.Entry;
+import ch.zli.m223.kti.punchclock.domain.Expense;
 import ch.zli.m223.kti.punchclock.service.ExpenseService;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/expenses")
 @Tag(name = "Expenses", description = "Handling of expenses")
@@ -12,4 +20,12 @@ public class ExpenseController {
 
     @Inject
     ExpenseService expenseService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @JsonIgnore
+    @Operation(summary = "List all Expenses", description = "return List of all Expenses")
+    public List<Expense> list() {
+        return expenseService.getAllExpenses();
+    }
 }
