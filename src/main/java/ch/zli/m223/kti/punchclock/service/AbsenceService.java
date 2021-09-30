adbf49820c3211c97467ba4ee3999617147ac8a8
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:37:11 +0200

    Inject Entity Manager for AbsenceService

--- src/main/java/ch/zli/m223/kti/punchclock/service/AbsenceService.java
+++ src/main/java/ch/zli/m223/kti/punchclock/service/AbsenceService.java
@@ -1,4 +1,10 @@
 package ch.zli.m223.kti.punchclock.service;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
 public class AbsenceService {
+
+    @Inject
+    private EntityManager entityManager;
 }
