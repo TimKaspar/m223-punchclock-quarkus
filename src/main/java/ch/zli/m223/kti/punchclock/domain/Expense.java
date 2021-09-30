61761dbdaef2ed096f830c0c769570f718c7aaa2
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:32:49 +0200

    added getter & setter to Expense

--- src/main/java/ch/zli/m223/kti/punchclock/domain/Expense.java
+++ src/main/java/ch/zli/m223/kti/punchclock/domain/Expense.java
@@ -7,4 +7,28 @@ public class Expense {
     private Double amount;
 
     private String receipt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(String receipt) {
+        this.receipt = receipt;
+    }
 }
