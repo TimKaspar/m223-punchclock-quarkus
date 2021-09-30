4d7ca4db89fc915449b258de864e696c9c030fc1
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:33:37 +0200

    added annotations to Expense

--- src/main/java/ch/zli/m223/kti/punchclock/domain/Expense.java
+++ src/main/java/ch/zli/m223/kti/punchclock/domain/Expense.java
@@ -1,11 +1,18 @@
 package ch.zli.m223.kti.punchclock.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Expense {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Double amount;
 
+    @Column(nullable = false)
     private String receipt;
 
     public Long getId() {
