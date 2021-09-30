7a956c91c7cc9b50d65c600560f36b2cadc22b12
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:59:16 +0200

    annotations for AbsenceController

--- src/main/java/ch/zli/m223/kti/punchclock/controller/AbsenceController.java
+++ src/main/java/ch/zli/m223/kti/punchclock/controller/AbsenceController.java
@@ -1,4 +1,10 @@
 package ch.zli.m223.kti.punchclock.controller;
 
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.ws.rs.Path;
+
+@Path("/absences")
+@Tag(name = "Absences", description = "Handling of absences")
 public class AbsenceController {
 }
