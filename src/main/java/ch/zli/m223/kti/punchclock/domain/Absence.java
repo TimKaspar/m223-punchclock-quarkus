7bde7ac03e05367a62b2cef654d9f85af1d65fe2
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:29:36 +0200

    added getter & setter

--- src/main/java/ch/zli/m223/kti/punchclock/domain/Absence.java
+++ src/main/java/ch/zli/m223/kti/punchclock/domain/Absence.java
@@ -9,4 +9,28 @@ public class Absence {
     private LocalDateTime start;
 
     private LocalDateTime end;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
 }
