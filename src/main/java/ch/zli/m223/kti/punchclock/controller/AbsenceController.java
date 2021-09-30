5f6e0497a81b6ee5f2c33daebe0ee6a90f806725
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 09:11:15 +0200

    created list() for AbsenceController

--- src/main/java/ch/zli/m223/kti/punchclock/controller/AbsenceController.java
+++ src/main/java/ch/zli/m223/kti/punchclock/controller/AbsenceController.java
@@ -1,10 +1,19 @@
 package ch.zli.m223.kti.punchclock.controller;
 
+import ch.zli.m223.kti.punchclock.domain.Absence;
+import ch.zli.m223.kti.punchclock.domain.Expense;
 import ch.zli.m223.kti.punchclock.service.AbsenceService;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/absences")
 @Tag(name = "Absences", description = "Handling of absences")
@@ -12,4 +21,13 @@ public class AbsenceController {
 
     @Inject
     AbsenceService absenceService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @JsonIgnore
+    @Operation(summary = "List all Absences", description = "return List of all Absences")
+    public List<Absence> list() {
+        return absenceService.getAllAbsences();
+    }
+
 }
