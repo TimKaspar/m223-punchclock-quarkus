9495e5c72ce94bb0ead0ae41d227045bb2b5b0b5
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:45:25 +0200

    created createAbsence()

--- src/main/java/ch/zli/m223/kti/punchclock/service/AbsenceService.java
+++ src/main/java/ch/zli/m223/kti/punchclock/service/AbsenceService.java
@@ -1,10 +1,20 @@
 package ch.zli.m223.kti.punchclock.service;
 
+import ch.zli.m223.kti.punchclock.domain.Absence;
+import ch.zli.m223.kti.punchclock.domain.Expense;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 public class AbsenceService {
 
     @Inject
     private EntityManager entityManager;
+
+    @Transactional
+    public Absence createAbsence(Absence absence) {
+        entityManager.persist(absence);
+        return absence;
+    }
 }
