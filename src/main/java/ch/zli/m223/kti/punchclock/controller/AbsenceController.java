891c628a49615829328d19209ca048a7195b9808
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 09:00:38 +0200

    inject AbsenceService in AbsenceController

--- src/main/java/ch/zli/m223/kti/punchclock/controller/AbsenceController.java
+++ src/main/java/ch/zli/m223/kti/punchclock/controller/AbsenceController.java
@@ -1,10 +1,15 @@
 package ch.zli.m223.kti.punchclock.controller;
 
+import ch.zli.m223.kti.punchclock.service.AbsenceService;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 
 @Path("/absences")
 @Tag(name = "Absences", description = "Handling of absences")
 public class AbsenceController {
+
+    @Inject
+    AbsenceService absenceService;
 }
