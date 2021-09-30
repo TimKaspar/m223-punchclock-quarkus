dbf007f3174d59a2766ac7fd199e0558a277cc76
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:28:42 +0200

    created attributes

--- src/main/java/ch/zli/m223/kti/punchclock/domain/Absence.java
+++ src/main/java/ch/zli/m223/kti/punchclock/domain/Absence.java
@@ -1,4 +1,12 @@
 package ch.zli.m223.kti.punchclock.domain;
 
+import java.time.LocalDateTime;
+
 public class Absence {
+
+    private Long id;
+
+    private LocalDateTime start;
+
+    private LocalDateTime end;
 }
