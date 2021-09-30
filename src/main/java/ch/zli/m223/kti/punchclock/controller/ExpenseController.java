c4a57d7afa5b9cc1359fe462f94a613d7d40ce83
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 09:00:01 +0200

    inject ExpenseService in ExpenseController

--- src/main/java/ch/zli/m223/kti/punchclock/controller/ExpenseController.java
+++ src/main/java/ch/zli/m223/kti/punchclock/controller/ExpenseController.java
@@ -1,10 +1,15 @@
 package ch.zli.m223.kti.punchclock.controller;
 
+import ch.zli.m223.kti.punchclock.service.ExpenseService;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 
 @Path("/expenses")
 @Tag(name = "Expenses", description = "Handling of expenses")
 public class ExpenseController {
+
+    @Inject
+    ExpenseService expenseService;
 }
