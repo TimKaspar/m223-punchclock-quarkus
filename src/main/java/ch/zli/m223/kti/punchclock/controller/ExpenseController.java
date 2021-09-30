c1ecf7ad11a2a5cf989c957c6219a8d796eef7a9
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:58:35 +0200

    annotations for ExpenseController

--- src/main/java/ch/zli/m223/kti/punchclock/controller/ExpenseController.java
+++ src/main/java/ch/zli/m223/kti/punchclock/controller/ExpenseController.java
@@ -1,4 +1,10 @@
 package ch.zli.m223.kti.punchclock.controller;
 
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.ws.rs.Path;
+
+@Path("/expenses")
+@Tag(name = "Expenses", description = "Handling of expenses")
 public class ExpenseController {
 }
