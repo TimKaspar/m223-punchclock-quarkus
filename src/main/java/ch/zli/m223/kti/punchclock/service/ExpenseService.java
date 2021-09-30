abbdecb37a483f8daaf1febf6ff91e80df6465bb
tim kaspar <tim2kaspar@gmail.com>
2021-09-30 08:36:58 +0200

    Inject Entity Manager for Expense

--- src/main/java/ch/zli/m223/kti/punchclock/service/ExpenseService.java
+++ src/main/java/ch/zli/m223/kti/punchclock/service/ExpenseService.java
@@ -1,4 +1,11 @@
 package ch.zli.m223.kti.punchclock.service;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
 public class ExpenseService {
+
+    @Inject
+    private EntityManager entityManager;
+
 }
